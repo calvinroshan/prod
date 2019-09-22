5e26b7dd0908c61ee5b67644a1fdc0484ed15c08
Your Name <you@example.com>
2019-09-22 16:51:33 +0530

    Updates

---
+++ precache-manifest.cd6ce54a5ca0bc5f6f9658f8261e297f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ed64c3b95cc52ffb1b9f3f7a09787a",
+    "url": "/calvinroshan/prod.git/index.html"
+  },
+  {
+    "revision": "71db295004627f954ecd",
+    "url": "/calvinroshan/prod.git/static/css/main.17f5729e.chunk.css"
+  },
+  {
+    "revision": "254bf41994287c643d78",
+    "url": "/calvinroshan/prod.git/static/js/2.1a5ecc84.chunk.js"
+  },
+  {
+    "revision": "71db295004627f954ecd",
+    "url": "/calvinroshan/prod.git/static/js/main.cc37314d.chunk.js"
+  },
+  {
+    "revision": "658ca4c6689d723c6ad3",
+    "url": "/calvinroshan/prod.git/static/js/runtime~main.362051b0.js"
+  }
+]);
